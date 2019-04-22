d2907401370d255b88334b8b2bc1c7f430c4651c
Minchan Kim <minchan@google.com>
2019-04-23 00:51:06 +0800

    mm: mm_event: add reclaim stat
    This patch adds page reclaim mm_event stat so that we could
    keep tracking [avg|max]_latency for the handling the event
    as well as count of the event.
    
    Direct reclaim latency is usually a most popular latency source
    caused by memory pressure so we need to track it down to hunt
    down application's jank problem.
    
    Bug: 80168800
    Bug: 116825053
    Test: boot
    Change-Id: I215c3972f76389404da7c4806a776bf753daac01
    Signed-off-by: Minchan Kim <minchan@google.com>
    (cherry picked from commit e07f7701a15fe04a237bb45e6adc8b0ee921e70e)
    Signed-off-by: Martin Liu <liumartin@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -3075,6 +3075,7 @@ out:
 unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 				gfp_t gfp_mask, nodemask_t *nodemask)
 {
+	ktime_t event_ts;
 	unsigned long nr_reclaimed;
 	struct scan_control sc = {
 		.nr_to_reclaim = SWAP_CLUSTER_MAX,
@@ -3096,6 +3097,7 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 	if (throttle_direct_reclaim(sc.gfp_mask, zonelist, nodemask))
 		return 1;
 
+	mm_event_start(&event_ts);
 	trace_mm_vmscan_direct_reclaim_begin(order,
 				sc.may_writepage,
 				sc.gfp_mask,
@@ -3104,6 +3106,7 @@ unsigned long try_to_free_pages(struct zonelist *zonelist, int order,
 	nr_reclaimed = do_try_to_free_pages(zonelist, &sc);
 
 	trace_mm_vmscan_direct_reclaim_end(nr_reclaimed);
+	mm_event_end(MM_RECLAIM, event_ts);
 
 	return nr_reclaimed;
 }
@@ -3614,6 +3617,7 @@ static int kswapd(void *p)
 	for ( ; ; ) {
 		bool ret;
 
+                ktime_t event_ts;
 		alloc_order = reclaim_order = pgdat->kswapd_order;
 		classzone_idx = kswapd_classzone_idx(pgdat, classzone_idx);
 
@@ -3648,9 +3652,13 @@ kswapd_try_sleep:
 		 */
 		trace_mm_vmscan_kswapd_wake(pgdat->node_id, classzone_idx,
 						alloc_order);
+
+		mm_event_start(&event_ts);
 		fs_reclaim_acquire(GFP_KERNEL);
 		reclaim_order = balance_pgdat(pgdat, alloc_order, classzone_idx);
 		fs_reclaim_release(GFP_KERNEL);
+		mm_event_end(MM_RECLAIM, event_ts);
+
 		if (reclaim_order < alloc_order)
 			goto kswapd_try_sleep;
 	}
