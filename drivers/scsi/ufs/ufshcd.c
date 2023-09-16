aosp-mirror/kernel_msm

scsi: ufs: allow read_16 and write_16 with proper string
This patch allows read_16/write_16 for IO stats, and give parsed opcode string
for Slow IOs.

Bug: 73896231
Change-Id: I7c6a92e26d70bdac9d92cb3018109598323a3d67
Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

drivers/scsi/ufs/ufshcd.c
@@ -58,6 +58,7 @@ static void ufshcd_log_slowio(struct ufs_hba *hba,
 	sector_t lba = -1;
 	int transfer_len = -1;
 	u8 opcode = 0xff;
+	char opcode_str[16];
 
 	if (likely(iotime_us < hba->slowio_us))
 		return;
@@ -66,7 +67,7 @@ static void ufshcd_log_slowio(struct ufs_hba *hba,
 
 	if (lrbp->cmd) {
 		opcode = (u8)(*lrbp->cmd->cmnd);
-		if (opcode == READ_10 || opcode == WRITE_10) {
+		if (is_read_opcode(opcode) || is_write_opcode(opcode)) {
 			if (lrbp->cmd->request && lrbp->cmd->request->bio)
 				lba = lrbp->cmd->request->bio->
 					bi_iter.bi_sector;
@@ -74,10 +75,11 @@ static void ufshcd_log_slowio(struct ufs_hba *hba,
 				lrbp->ucd_req_ptr->sc.exp_data_transfer_len);
 		}
 	}
+	snprintf(opcode_str, 16, "%02x: %s", opcode, parse_opcode(opcode));
 	dev_err_ratelimited(hba->dev,
-		"Slow UFS (%lld): time = %lld us, opcode = %02x, lba = %ld, "
-		"len = %d\n", hba->slowio_cnt, iotime_us, opcode, lba,
-		transfer_len);
+		"Slow UFS (%lld): time = %lld us, opcode = %16s, lba = %ld, "
+		"len = %d\n",
+		hba->slowio_cnt, iotime_us, opcode_str, lba, transfer_len);
 }
 
 #ifdef CONFIG_DEBUG_FS
@@ -214,20 +216,15 @@ update_io_stat(struct ufs_hba *hba, int tag, int is_start)
 	if (!lrbp->cmd)
 		return;
 	opcode = (u8)(*lrbp->cmd->cmnd);
-	if (opcode != READ_10 && opcode != WRITE_10)
+	if (!is_read_opcode(opcode) && is_write_opcode(opcode))
 		return;
 
 	transfer_len = be32_to_cpu(lrbp->ucd_req_ptr->sc.exp_data_transfer_len);
 
 	__update_io_stat(hba, &hba->ufs_stats.io_readwrite, transfer_len,
 			is_start);
-	if (opcode == READ_10) {
-		__update_io_stat(hba, &hba->ufs_stats.io_read, transfer_len,
-				is_start);
-	} else {
-		__update_io_stat(hba, &hba->ufs_stats.io_write, transfer_len,
-				is_start);
-	}
+	__update_io_stat(hba, is_read_opcode(opcode) ? &hba->ufs_stats.io_read:
+			&hba->ufs_stats.io_write, transfer_len, is_start);
 }
 
 #else
@@ -785,10 +782,10 @@ static inline void ufshcd_cond_add_cmd_trace(struct ufs_hba *hba,
 
 	if (lrbp->cmd) { /* data phase exists */
 		opcode = (u8)(*lrbp->cmd->cmnd);
-		if ((opcode == READ_10) || (opcode == WRITE_10)) {
+		if (is_read_opcode(opcode) || is_write_opcode(opcode)) {
 			/*
-			 * Currently we only fully trace read(10) and write(10)
-			 * commands
+			 * Currently we only fully trace read(10), write(10),
+			 * read(16), and write(16) commands
 			 */
 			if (lrbp->cmd->request && lrbp->cmd->request->bio)
 				lba =

Moto-Liber/msm-4.14

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

drivers/scsi/ufs/ufshcd.c
@@ -55,6 +55,7 @@ static void ufshcd_log_slowio(struct ufs_hba *hba,
 	sector_t la = -1;
 	int transfer_len = -1;
 	u8 opcode = 0xff;
+	char opcode_str[16];
 
 	if (likely(iotime_us < hba->slowio_us))
 		return;
@@ -63,17 +64,18 @@ static void ufshcd_log_slowio(struct ufs_hba *hba,
 
 	if (lrbp->cmd) {
 		opcode = (u8)(*lrbp->cmd->cmnd);
-		if (opcode == READ_10 || opcode == WRITE_10) {
+		if (is_read_opcode(opcode) || is_write_opcode(opcode)) {
 			if (lrbp->cmd->request && lrbp->cmd->request->bio)
 				la = lrbp->cmd->request->bio->bi_iter.bi_sector;
 			transfer_len = be32_to_cpu(
 				lrbp->ucd_req_ptr->sc.exp_data_transfer_len);
 		}
 	}
+	snprintf(opcode_str, 16, "%02x: %s", opcode, parse_opcode(opcode));
 	dev_err_ratelimited(hba->dev,
-		"Slow UFS (%lld): time = %lld us, opcode = %02x, lba = %ld, "
-		"len = %d\n", hba->slowio_cnt, iotime_us, opcode, la,
-		transfer_len);
+		"Slow UFS (%lld): time = %lld us, opcode = %16s, lba = %ld, "
+		"len = %d\n",
+		hba->slowio_cnt, iotime_us, opcode_str, la, transfer_len);
 }
 
 #ifdef CONFIG_DEBUG_FS
@@ -210,20 +212,15 @@ update_io_stat(struct ufs_hba *hba, int tag, int is_start)
 	if (!lrbp->cmd)
 		return;
 	opcode = (u8)(*lrbp->cmd->cmnd);
-	if (opcode != READ_10 && opcode != WRITE_10)
+	if (!is_read_opcode(opcode) && is_write_opcode(opcode))
 		return;
 
 	transfer_len = be32_to_cpu(lrbp->ucd_req_ptr->sc.exp_data_transfer_len);
 
 	__update_io_stat(hba, &hba->ufs_stats.io_readwrite, transfer_len,
 			is_start);
-	if (opcode == READ_10) {
-		__update_io_stat(hba, &hba->ufs_stats.io_read, transfer_len,
-				is_start);
-	} else {
-		__update_io_stat(hba, &hba->ufs_stats.io_write, transfer_len,
-				is_start);
-	}
+	__update_io_stat(hba, is_read_opcode(opcode) ? &hba->ufs_stats.io_read:
+			&hba->ufs_stats.io_write, transfer_len, is_start);
 }
 
 #else
@@ -903,10 +900,10 @@ static inline void ufshcd_cond_add_cmd_trace(struct ufs_hba *hba,
 
 	if (lrbp->cmd) { /* data phase exists */
 		opcode = (u8)(*lrbp->cmd->cmnd);
-		if ((opcode == READ_10) || (opcode == WRITE_10)) {
+		if (is_read_opcode(opcode) || is_write_opcode(opcode)) {
 			/*
-			 * Currently we only fully trace read(10) and write(10)
-			 * commands
+			 * Currently we only fully trace read(10), write(10),
+			 * read(16), and write(16) commands
 			 */
 			if (lrbp->cmd->request && lrbp->cmd->request->bio)
 				lba =